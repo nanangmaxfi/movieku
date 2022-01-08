9f9915465a20e1888f08306ed1d2db99c7287019
Nanang Mubarok <nanang_mubarok@saranaonline.com>
2022-01-08 08:14:51 +0700

    add activity detail and main

--- app/src/main/java/id/nanangmaxfi/movieku/ui/main/MainActivity.kt
+++ app/src/main/java/id/nanangmaxfi/movieku/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.nanangmaxfi.movieku
+package id.nanangmaxfi.movieku.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.nanangmaxfi.movieku.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
