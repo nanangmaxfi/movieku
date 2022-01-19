0b9bdc8b90b7066eb66ffe5a15c1978a5f0b4e03
Nanang Mubarok <nanang_mubarok@saranaonline.com>
2022-01-19 10:07:42 +0700

    add task for subs 2

--- core/src/main/java/id/nanangmaxfi/movieku/core/di/CoreModule.kt
+++ core/src/main/java/id/nanangmaxfi/movieku/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import id.nanangmaxfi.movieku.core.data.source.remote.RemoteDataSource
 import id.nanangmaxfi.movieku.core.data.source.remote.network.ApiService
 import id.nanangmaxfi.movieku.core.domain.repository.IMovieRepository
 import id.nanangmaxfi.movieku.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,19 +23,28 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("movie".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "Movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner =  CertificatePinner.Builder()
+            .add(hostname,"sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
